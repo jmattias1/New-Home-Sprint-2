4c55927a99a02db1ca330d0c11675603b1e0ed6e
jmattias1 <jmattias1@hotmail.com>
2022-07-17 22:15:57 -0300

    add html y css

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const port = 3030;
 
 app.use (express.static('public'));
 
-app.listen (port, () => console.log(`Server running in port: https://localhost:${port}`));
-
 app.get ('/', (req,res) => res.sendFile (path.join(__dirname, 'views', 'index.html')))
-app.get ('/about', (req,res) => res.sendFile (path.join(__dirname, 'views', 'login.html')))
-app.get ('/contact', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productCart.html')))
-app.get ('/music', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productDetail.html')))
-app.get ('/music', (req,res) => res.sendFile (path.join(__dirname, 'views', 'register.html')))
+app.get ('/login', (req,res) => res.sendFile (path.join(__dirname, 'views', 'login.html')))
+app.get ('/productCart', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productCart.html')))
+app.get ('/productDetail', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productDetail.html')))
+app.get ('/register', (req,res) => res.sendFile (path.join(__dirname, 'views', 'register.html')))
+
+app.listen (port, () => console.log(`Server running in port: http://localhost:` + port));
