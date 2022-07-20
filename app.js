af3c726cfa0e55206300d4fc03b4206e79af2321
maxii73 <athousandsuns73@gmail.com>
2022-07-20 13:47:05 -0300

    add funcionalidades

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3030;
 app.use (express.static('public'));
 
 app.get ('/', (req,res) => res.sendFile (path.join(__dirname, 'views', 'index.html')))
+app.get ('/index', (req,res) => res.sendFile (path.join(__dirname, 'views', 'index.html')))
 app.get ('/login', (req,res) => res.sendFile (path.join(__dirname, 'views', 'login.html')))
 app.get ('/productCart', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productCart.html')))
 app.get ('/productDetail', (req,res) => res.sendFile (path.join(__dirname, 'views', 'productDetail.html')))
